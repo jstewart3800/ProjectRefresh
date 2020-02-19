12f76f6d8777c12df9fbac7077ce0da1d4bebd7a
Jon Stewart <56611713+jstewart3800@users.noreply.github.com>
2020-02-18 19:57:44 -0800

    Setup head, main & menu base style, some routing

--- ProjectRefreshSite/src/app/app-routing.module.ts
+++ ProjectRefreshSite/src/app/app-routing.module.ts
@@ -1,11 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { MenuComponent } from './views/menu/menu.component';
+import { MainComponent } from './views/main/main.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', redirectTo: '/main', pathMatch: 'full'},
+  { path: 'main', component: MainComponent},
+  { path: 'menu', component: MenuComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+export const routingComponents = [
+  MenuComponent,
+  MainComponent
+];
--- ProjectRefreshSite/src/app/app.module.ts
+++ ProjectRefreshSite/src/app/app.module.ts
@@ -1,20 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './views/header/header.component';
 import { MainComponent } from './views/main/main.component';
+import { MenuComponent } from './views/menu/menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    MainComponent
+    MainComponent,
+    MenuComponent,
+    routingComponents
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
